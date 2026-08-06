b1560db144a848d24dacba7d80e7a657b6bd3c98
Saurabh Hasabe <saurya1729@gmail.com>
2026-08-06 19:31:35 +0530

    Client and Server Side Data Validation

--- app.js
+++ app.js
@@ -4,85 +4,138 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const app = express();
 const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/atithi";
 
-app.engine("ejs",ejsMate);
+app.engine("ejs", ejsMate);
 
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.listen(port,()=> {
-    console.log(`Listning to The Port : ${port}`);
+app.listen(port, () => {
+  console.log(`Listning to The Port : ${port}`);
 });
 
 main()
-    .then(() => {console.log("Connection Successful");})
-    .catch((err) => {console.log(err);});
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 // Root Route
-app.get("/",(req,res) => {
-    res.send("Working Root node");
+app.get("/", (req, res) => {
+  res.send("Working Root node");
 });
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 
 // Index Route
-app.get("/listings",async (req,res) => {
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
     let allListing = await Listing.find();
-    res.render("listings/index.ejs",{allListing});
-});
+    res.render("listings/index.ejs", { allListing });
+  }),
+);
 
 // New Route
-app.get("/listings/new", (req,res)=> {
-    res.render("listings/new.ejs");
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 
 // Create Route
-app.post("/listings", async (req,res)=> {
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    // if (result.error) {
+    //   throw new ExpressError(400, result.err);
+    // }
+    // if (!req.body.listing) {
+    //   throw new ExpressError(400, "Send valid data for listing");
+    // }
     let newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+  }),
+);
 
 // Edit Route
-app.get("/listings/:id/edit", async (req,res)=> {
-    let {id} = req.params;
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-});
+    res.render("listings/edit.ejs", { listing });
+  }),
+);
 
-// Update Route 
-app.put("/listings/:id", async(req,res)=> {
-    let {id} = req.params;
-    console.log(req.body.listing);
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+// Update Route
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    // if (!req.body.listing) {
+    //   throw new ExpressError(400, "Send valid data for listing");
+    // }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-});
+  }),
+);
 
 // Delete Route
-app.delete("/listings/:id",async (req,res) => {
-    let {id} = req.params;
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+  }),
+);
 
-// Show Route 
-app.get("/listings/:id", async (req,res) => {
-    let {id} = req.params;
+// Show Route
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let listing = await Listing.findById(id);
-    console.log(listing);
-    res.render("listings/show.ejs",{listing});
+    res.render("listings/show.ejs", { listing });
+  }),
+);
+
+app.all("/*splat", (req, res, next) => {
+  next(new ExpressError(404, "Page Not found !"));
 });
 
-
-
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went Wrong !" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
+});
--- models/listing.js
+++ models/listing.js
@@ -2,30 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title : {
-        type : String,
-        required : true
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
     },
-    description : String,
-    image : {
-        filename : {
-            type: String,
-            default: "listingimage"
-        },
-        url : {
-            type : String,
-            default : "https://images.unsplash.com/photo-1782392487647-7fee9715d1f5?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            set : (v) => {
-               return v === " " 
-               ? "https://images.unsplash.com/photo-1782392487647-7fee9715d1f5?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-               :v;
-            }
-        }       
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1782392487647-7fee9715d1f5?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) => {
+        if (!v || v.trim() === "") {
+          return "https://images.unsplash.com/photo-1782392487647-7fee9715d1f5?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+        }
+        return v;
+      },
     },
-    price :Number,
-    location : String,
-    country : String
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
