edba32015af22a73205a2c16e61f9fa4fc65896f
Saurabh Hasabe <saurya1729@gmail.com>
2026-08-31 00:25:51 +0530

    Implemented Signup & Login Module

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ const ExpressError = require("./utils/ExpressError.js");
 
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/atithi";
@@ -59,16 +63,24 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-// Listings Routes
-app.use("/listings", listings);
-// Reviews Routes
-app.use("/listings/:id/reviews", reviews);
+// Listing Routes
+app.use("/listings", listingRouter);
+// Review Routes
+app.use("/listings/:id/reviews", reviewRouter);
+// User Routes
+app.use("/", userRouter);
 
 // Error Handler Middlewares
 app.all("/*splat", (req, res, next) => {
